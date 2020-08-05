715a63665a67522ecd129146a1de68ae16a4a503
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-05 01:42:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,11 @@ class Student
   end
 
   def self.create_table
-    sql <<= SQL
-
-
+    sql = <<-SQL
+    CREATE TABLE students
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
     SQL
   end
   # Remember, you can access your database connection anywhere in this class
